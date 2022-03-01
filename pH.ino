7847176373bad62ad227f243a6e6491e03b30f54
giu11 <85152594+giu11@users.noreply.github.com>
2022-03-01 23:20:27 +0100

    Update and rename sketch_feb12a.ino to pH.ino

---
+++ pH.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(9600);  
+  Serial.println("PH meter");
+}
+
+float misura;
+
+void loop() {
+  for(int i=0; i<10; i++) { 
+    misura += analogRead(A0);
+    delay(10);
+  }
+
+  misura = misura / 10.0;  
+  float ph = (float)misura * (5.0/1024);
+  Serial.println(ph);
+  ph = 3.45 * ph;  
+  Serial.print("    pH:");  
+  Serial.print(ph, 2);
+  Serial.println(" ");
+  delay(1000);
+
+}
